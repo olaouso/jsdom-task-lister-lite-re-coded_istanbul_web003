22a646e820bbb57fb50ec4d9fe225b5bb5dbac07
Ola Ouso <ouso.ola@gmail.com>
2020-10-18 17:19:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {let tasks=[];
       tasks.push(document.getElementById("new-task-description").value);
       let id = tasks.length;
 
-      document.getElementById("tasks").innerHTML +=  ` 
+      document.getElementById("tasks").innerHTML +=` 
       <li id= li${id}>
       ${tasks[tasks.length-1]}
       <button class = remove id = ${id}>Remove</button>
