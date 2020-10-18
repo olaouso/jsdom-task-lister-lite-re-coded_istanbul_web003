f992be90538a410422527db19231f707ca9f4daf
Ola Ouso <ouso.ola@gmail.com>
2020-10-18 17:16:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  
+}
+   let tasks=[];
+  document.getElementById("create-task-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+      tasks.push(document.getElementById("new-task-description").value);
+      let id = tasks.length;
+
+      document.getElementById("tasks").innerHTML +=  ` 
+      <li id= li${id}>
+      ${tasks[tasks.length-1]}
+      <button class = remove id = ${id}>Remove</button>
+    </li>`
+
+  })
