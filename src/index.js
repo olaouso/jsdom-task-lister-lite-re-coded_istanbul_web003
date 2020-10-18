83b55a2b25e075b1fb2ccee538ff1cf8da167553
your_name <your_email@example.com>
2020-10-18 20:47:12 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const input = document.getElementById('create-task-form')
+  const output = document.getElementById('tasks');
+
+   input.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const list = document.getElementById('new-task-description').value;
+
+    output.innerHTML += `<li>${list}</li>`
+     input.reset()
+  })
 });
